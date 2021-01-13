9d0c746845a474e073c72a21d61398afcbbb9c99
Ameer Taweel <ameertaweel2002@gmail.com>
2021-01-13 15:43:03 +0200

    Solve inheritance q2

--- inheritance_q2.py
+++ inheritance_q2.py
@@ -1,23 +1,34 @@
+import math
+
+
 class Polygon:
-  def __init__(self, no_of_sides):
-    self.n = no_of_sides
-    self.sides = [0 for i in range(no_of_sides)]
+    def __init__(self, no_of_sides):
+        self.n = no_of_sides
+        self.sides = [0 for i in range(no_of_sides)]
 
-  def inputSides(self):
-    # TODO
+    def inputSides(self):
+        for i in range(self.n):
+            self.sides[i] = float(input(f"Enter side {i}: "))
 
-  def dispSides(self):
-    # TODO
+    def dispSides(self):
+        for i in range(len(self.sides)):
+            print(f"Side {i} is {self.sides[i]}")
+
 
 class Triangle(Polygon):
-  def __init__(self):
-    Polygon.__init__(self, 3)
+    def __init__(self):
+        Polygon.__init__(self, 3)
 
-  def findArea(self):
-    # TODO
+    def findArea(self):
+        """
+        Uses Heron's formula for the area of a triangle
+        """
+        a, b, c = self.sides
+        p = (a + b + c) / 2
+        return math.sqrt(p * (p - a) * (p - b) * (p - c))
 
 
 t = Triangle()
 t.inputSides()
 t.dispSides()
-t.findArea()
+print(f"The area of the triangle is {t.findArea()}")
